6d056b8363d3e263a0ff4307b36fe9b2f82c23bc
GABRIEL MATURANA <gabriel.maturana@uc.cl>
2025-05-14 01:08:29 +0000

    cambio ubicacion

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,8 @@
 export const COORDINATOR_PASSWORD = "coord_password123";
 export const ADMIN_PASSWORD = "admin_password123";
 
-export const JUMBO_LATITUDE = -33.4170; // Example: Jumbo Costanera Center
-export const JUMBO_LONGITUDE = -70.6061;
+export const JUMBO_LATITUDE = -33.564309; 
+export const JUMBO_LONGITUDE = -70.680308;
 export const MAX_DISTANCE_METERS = 50;
 
 export const AUTH_TOKEN_KEY = "jumbo_dispatch_auth_token";
